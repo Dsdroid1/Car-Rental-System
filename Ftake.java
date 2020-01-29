0f738dea8de7f1553fd00a60c959aeff52e9a66b
Dsdroid1 <dhruvsharma2031@gmail.com>
2020-01-29 17:39:05 +0530

    Improved feedback as an interface

--- Ftake.java
+++ Ftake.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 interface feedback_setup
 {
-//    int max_rating=5;
-   // int min_rating=0;
+    int max_rating=5;
+    int min_rating=0;
     public void Takefeedback(feedback e);
     public void Showfeedback(feedback e);
 }
@@ -16,21 +16,21 @@ class Ftake implements feedback_setup
         System.out.println("Your booking has been completed,please take sometime to give us your valuable feedback!!");
         Scanner s=new Scanner(System.in);
         System.out.println("------------------------------------------");
-        System.out.println("1.How was our user interface(from 0-5):");
+        System.out.println("1.How was our user interface(from "+min_rating+"-"+max_rating+"):");
         e.f1=s.nextInt();
         while(e.f1<0||e.f1>5)
         {
             System.out.println("Please enter valid number!!");
             e.f1=s.nextInt();
         }
-        System.out.println("2.How helpful was this app to you(from 0-5):");
+        System.out.println("2.How helpful was this app to you:(from "+min_rating+"-"+max_rating+"):");
         e.f2=s.nextInt();
         while(e.f2<0||e.f2>5)
         {
             System.out.println("Please enter valid number!!");
             e.f2=s.nextInt();
         }
-        System.out.println("3.How likely are you to recommend us to a friend(from 0-5):");
+        System.out.println("3.How likely are you to recommend us to a friend(from "+min_rating+"-"+max_rating+"):");
         e.f3=s.nextInt();
         while(e.f3<0||e.f3>5)
         {
